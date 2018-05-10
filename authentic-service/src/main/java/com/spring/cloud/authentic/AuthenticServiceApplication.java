34c42ca429922d1171065ee5c2e07f54d67b5c02
wmang <itfast@yeah.net>
2018-05-10 09:12:12 +0800

    修复认证服务

--- authentic-service/src/main/java/com/spring/cloud/authentic/AuthenticServiceApplication.java
+++ authentic-service/src/main/java/com/spring/cloud/authentic/AuthenticServiceApplication.java
@@ -1,13 +1,16 @@
 package com.spring.cloud.authentic;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableScheduling
-//@EnableCaching
+@EnableEurekaClient
+@MapperScan("com.spring.cloud.authentic.dao")
 public class AuthenticServiceApplication {
 
 	public static void main(String[] args) {
